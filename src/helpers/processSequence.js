2c5b668347134f7a1c1993270b4d1af1065bf73a
Samoylov_Eugene <samyelson.00@yandex.ru>
2025-07-13 20:28:12 +0300

    Functional versions

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -14,65 +14,151 @@
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
+
 import Api from "../tools/api";
 
 const api = new Api();
 
-function isValid(value) {
-  const len = value.length;
-  console.log(+value);
-  const symbs = "0123456789.".split("");
-  let dotsCount = 0;
-  return (
-    value[0] !== "." &&
-    (value[0] !== "0" || value[1] === ".") &&
-    value[len - 1] !== "." &&
-    len < 10 &&
-    len > 2 &&
-    value.split("").every((a) => {
-      if (a === ".") dotsCount++;
-      return symbs.includes(a);
-    }) &&
-    dotsCount < 2 &&
-    +value >= 0.5
-  );
-}
+const lengthBetween = (min, max) => value => 
+  value.length > min && value.length < max;
 
-function roundString(string) {
-  return Math.round(+string);
-}
+const isValidChars = value => 
+  /^[0-9.]+$/.test(value);
 
+const isValidNumber = value => {
+  if (value.startsWith('0') && !value.startsWith('0.')) return false;
+  if (value.endsWith('.')) return false;
+  
+  const num = parseFloat(value);
+  return num > 0 && !isNaN(num);
+};
+
+const countDots = value => 
+  value.split('').filter(char => char === '.').length;
+
+const isValid = value => 
+  lengthBetween(2, 10)(value) &&
+  isValidChars(value) &&
+  countDots(value) <= 1 &&
+  isValidNumber(value);
+
+const log = (value, writeLog) => {
+  writeLog(value);
+  return value;
+};
+
+const round = value => Math.round(Number(value));
+
+const toBinary = number => 
+  api.get("https://api.tech/numbers/base")({ 
+    from: 10, 
+    to: 2, 
+    number 
+  });
+
+const getLength = str => str.length;
+
+const square = num => num ** 2;
+
+const mod3 = num => num % 3;
+
+const getAnimal = id => 
+  api.get(`https://animals.tech/${id}`)({});
 
 const processSequence = ({ value, writeLog, handleSuccess, handleError }) => {
-  writeLog(value);
-
-  if (!isValid(value)) {
-    handleError("ValidationError");
-    return;
-  }
-
-  const intValue = roundString(value);
-  writeLog(intValue);
-
-  api
-    .get("https://api.tech/numbers/base", {
-      from: 10,
-      to: 2,
-      number: intValue,
+  Promise.resolve(value)
+    .then(val => {
+      writeLog(val);
+      return val;
     })
-    .then(({ result }) => {
-      writeLog(result);
-      let num = result.length;
-      writeLog(num);
-      num = num ** 2;
-      writeLog(num);
-      num = num % 3;
-      writeLog(num);
-      return num;
+    .then(val => {
+      if (!isValid(val)) {
+        throw new Error('ValidationError');
+      }
+      return val;
     })
-    .then(num => api.get(`https://animals.tech/${num}`)(num))
+    .then(round)
+    .then(num => log(num, writeLog))
+    .then(toBinary)
+    .then(({ result }) => log(result, writeLog))
+    .then(getLength)
+    .then(len => log(len, writeLog))
+    .then(square)
+    .then(sq => log(sq, writeLog))
+    .then(mod3)
+    .then(mod => log(mod, writeLog))
+    .then(getAnimal)
     .then(({ result }) => handleSuccess(result))
-    .catch(({ result }) => handleError(result));
+    .catch(error => {
+      handleError(error.message === 'ValidationError' 
+        ? 'ValidationError' 
+        : error.result || 'Unknown error'
+      );
+    });
 };
 
 export default processSequence;
+
+
+// import Api from "../tools/api";
+
+// const api = new Api();
+
+// function isValid(value) {
+//   const len = value.length;
+//   console.log(+value);
+//   const symbs = "0123456789.".split("");
+//   let dotsCount = 0;
+//   return (
+//     value[0] !== "." &&
+//     (value[0] !== "0" || value[1] === ".") &&
+//     value[len - 1] !== "." &&
+//     len < 10 &&
+//     len > 2 &&
+//     value.split("").every((a) => {
+//       if (a === ".") dotsCount++;
+//       return symbs.includes(a);
+//     }) &&
+//     dotsCount < 2 &&
+//     +value >= 0.5
+//   );
+// }
+
+// function roundString(string) {
+//   return Math.round(+string);
+// }
+
+
+// const processSequence = ({ value, writeLog, handleSuccess, handleError }) => {
+//   writeLog(value);
+
+//   if (!isValid(value)) {
+//     handleError("ValidationError");
+//     return;
+//   }
+
+//   const intValue = roundString(value);
+//   writeLog(intValue);
+
+//   api
+//     .get("https://api.tech/numbers/base", {
+//       from: 10,
+//       to: 2,
+//       number: intValue,
+//     })
+//     .then(({ result }) => {
+//       writeLog(result);
+//       let num = result.length;
+//       writeLog(num);
+//       num = num ** 2;
+//       writeLog(num);
+//       num = num % 3;
+//       writeLog(num);
+//       return num;
+//     })
+//     .then(num => api.get(`https://animals.tech/${num}`)(num))
+//     .then(({ result }) => handleSuccess(result))
+//     .catch(({ result }) => handleError(result));
+// };
+
+// export default processSequence;
--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -13,93 +13,169 @@
  * Если какие либо функции написаны руками (без использования библиотек) это не является ошибкой
  */
 
-// 1. Красная звезда, зеленый квадрат, все остальные белые.
-export const validateFieldN1 = ({star, square, triangle, circle}) => {
-    if (triangle !== 'white' || circle !== 'white') {
-        return false;
-    }
-
-    return star === 'red' && square === 'green';
-};
-
-// 2. Как минимум две фигуры зеленые.
-export const validateFieldN2 = ({star, square, triangle, circle}) => {
-    let arr = [star, square, triangle, circle];
-    let counter = arr.reduce((sum, a) => a === 'green' ? sum +=1 : sum, 0);
-    if(counter >= 2) return true;
-    return false;
-};
-
-
-// 3. Количество красных фигур равно кол-ву синих.
-export const validateFieldN3 = ({star, square, triangle, circle}) => {
-    let arr = [star, square, triangle, circle];
-    let blue = arr.reduce((sum, a) => a === 'blue' ? sum +=1 : sum, 0);
-    let red = arr.reduce((sum, a) => a === 'red' ? sum +=1 : sum, 0);
-    if(blue === red) return true;
-    return false;
-};
-
-// 4. Синий круг, красная звезда, оранжевый квадрат треугольник любого цвета
-export const validateFieldN4 = ({star, square, circle}) => {
-    return circle === "blue" && star === "red" && square === "orange";
-};
-
-// 5. Три фигуры одного любого цвета кроме белого (четыре фигуры одного цвета – это тоже true).
-export const validateFieldN5 = ({star, square, triangle, circle}) => {
-    const colors = {};
-    const arr = [star, square, triangle, circle];
-    arr.forEach((a) => {
-        if(a in colors){
-            colors[a]++;
-        } else {
-            colors[a] = 1;
-        }
-    })
-    let counter = 0;
-    for(let color in colors){
-        if(color !== "white" && colors[color] > counter){
-            counter = colors[color];
-        }
-    }
-    return counter >= 3;
-};
-
-// 6. Ровно две зеленые фигуры (одна из зелёных – это треугольник), плюс одна красная. Четвёртая оставшаяся любого доступного цвета, но не нарушающая первые два условия
-export const validateFieldN6 = ({star, square, triangle, circle}) => {
-    if(triangle !== "green") return false;
-
-    const colors = {};
-    const arr = [star, square, triangle, circle];
-    arr.forEach((a) => {
-        if(a in colors){
-            colors[a]++;
-        } else {
-            colors[a] = 1;
-        }
-    })
-    return colors["green"] >= 2 && colors["red"] === 1;
-};
-
-// 7. Все фигуры оранжевые.
-export const validateFieldN7 = ({star, square, triangle, circle}) => {
-    const color = 'orange';
-    return circle === color && star === color && square === color && triangle === color;
-};
-
-// 8. Не красная и не белая звезда, остальные – любого цвета.
-export const validateFieldN8 = ({star}) => {
-    return star !== 'white' && star !== "red";
-};
-
-// 9. Все фигуры зеленые.
-export const validateFieldN9 = ({star, square, triangle, circle}) => {
-    let color = 'green';
-    return circle === color && star === color && square === color && triangle === color;
-};
-
-// 10. Треугольник и квадрат одного цвета (не белого), остальные – любого цвета
-export const validateFieldN10 = ({square, triangle}) => {
-    return triangle === square;
-};
  
+import * as R from 'ramda';
+
+// 1. Красная звезда, зеленый квадрат, все остальные белые
+export const validateFieldN1 = R.where({
+  star: R.equals('red'),
+  square: R.equals('green'),
+  triangle: R.equals('white'),
+  circle: R.equals('white')
+});
+
+// 2. Как минимум две фигуры зеленые
+export const validateFieldN2 = R.pipe(
+  R.values,
+  R.filter(R.equals('green')),
+  R.length,
+  R.lte(2)
+);
+
+// 3. Количество красных фигур равно кол-ву синих
+export const validateFieldN3 = R.converge(
+  R.equals, [
+    R.pipe(R.values, R.filter(R.equals('red')), R.length),
+    R.pipe(R.values, R.filter(R.equals('blue')), R.length)
+  ]
+);
+
+// 4. Синий круг, красная звезда, оранжевый квадрат, треугольник любого цвета
+export const validateFieldN4 = R.where({
+  circle: R.equals('blue'),
+  star: R.equals('red'),
+  square: R.equals('orange')
+});
+
+// 5. Три+ фигуры одного цвета (кроме белого)
+export const validateFieldN5 = R.pipe(
+  R.values,
+  R.reject(R.equals('white')),
+  R.countBy(R.identity),
+  R.values,
+  R.any(R.lte(3))
+);
+
+// 6. Ровно две зеленые (треугольник - зеленый), одна красная
+export const validateFieldN6 = R.allPass([
+  R.propEq('triangle', 'green'),
+  R.pipe(R.values, R.filter(R.equals('green')), R.length, R.equals(2)),
+  R.pipe(R.values, R.filter(R.equals('red')), R.length, R.equals(1))
+]);
+
+// 7. Все фигуры оранжевые
+export const validateFieldN7 = R.pipe(
+  R.values,
+  R.all(R.equals('orange'))
+);
+
+// 8. Звезда: не красная и не белая
+export const validateFieldN8 = R.pipe(
+  R.prop('star'),
+  R.both(R.complement(R.equals('red')), R.complement(R.equals('white')))
+);
+
+// 9. Все фигуры зеленые
+export const validateFieldN9 = R.pipe(
+  R.values,
+  R.all(R.equals('green'))
+);
+
+// 10. Треугольник и квадрат одного цвета (не белого)
+export const validateFieldN10 = R.allPass([
+  R.converge(R.equals, [
+    R.prop('triangle'),
+    R.prop('square')
+  ]),
+  R.pipe(R.prop('triangle'), R.complement(R.equals('white')))
+]);
+
+// // 1. Красная звезда, зеленый квадрат, все остальные белые.
+// export const validateFieldN1 = ({star, square, triangle, circle}) => {
+//     if (triangle !== 'white' || circle !== 'white') {
+//         return false;
+//     }
+
+//     return star === 'red' && square === 'green';
+// };
+
+// // 2. Как минимум две фигуры зеленые.
+// export const validateFieldN2 = ({star, square, triangle, circle}) => {
+//     let arr = [star, square, triangle, circle];
+//     let counter = arr.reduce((sum, a) => a === 'green' ? sum +=1 : sum, 0);
+//     if(counter >= 2) return true;
+//     return false;
+// };
+
+
+// // 3. Количество красных фигур равно кол-ву синих.
+// export const validateFieldN3 = ({star, square, triangle, circle}) => {
+//     let arr = [star, square, triangle, circle];
+//     let blue = arr.reduce((sum, a) => a === 'blue' ? sum +=1 : sum, 0);
+//     let red = arr.reduce((sum, a) => a === 'red' ? sum +=1 : sum, 0);
+//     if(blue === red) return true;
+//     return false;
+// };
+
+// // 4. Синий круг, красная звезда, оранжевый квадрат треугольник любого цвета
+// export const validateFieldN4 = ({star, square, circle}) => {
+//     return circle === "blue" && star === "red" && square === "orange";
+// };
+
+// // 5. Три фигуры одного любого цвета кроме белого (четыре фигуры одного цвета – это тоже true).
+// export const validateFieldN5 = ({star, square, triangle, circle}) => {
+//     const colors = {};
+//     const arr = [star, square, triangle, circle];
+//     arr.forEach((a) => {
+//         if(a in colors){
+//             colors[a]++;
+//         } else {
+//             colors[a] = 1;
+//         }
+//     })
+//     let counter = 0;
+//     for(let color in colors){
+//         if(color !== "white" && colors[color] > counter){
+//             counter = colors[color];
+//         }
+//     }
+//     return counter >= 3;
+// };
+
+// // 6. Ровно две зеленые фигуры (одна из зелёных – это треугольник), плюс одна красная. Четвёртая оставшаяся любого доступного цвета, но не нарушающая первые два условия
+// export const validateFieldN6 = ({star, square, triangle, circle}) => {
+//     if(triangle !== "green") return false;
+
+//     const colors = {};
+//     const arr = [star, square, triangle, circle];
+//     arr.forEach((a) => {
+//         if(a in colors){
+//             colors[a]++;
+//         } else {
+//             colors[a] = 1;
+//         }
+//     })
+//     return colors["green"] >= 2 && colors["red"] === 1;
+// };
+
+// // 7. Все фигуры оранжевые.
+// export const validateFieldN7 = ({star, square, triangle, circle}) => {
+//     const color = 'orange';
+//     return circle === color && star === color && square === color && triangle === color;
+// };
+
+// // 8. Не красная и не белая звезда, остальные – любого цвета.
+// export const validateFieldN8 = ({star}) => {
+//     return star !== 'white' && star !== "red";
+// };
+
+// // 9. Все фигуры зеленые.
+// export const validateFieldN9 = ({star, square, triangle, circle}) => {
+//     let color = 'green';
+//     return circle === color && star === color && square === color && triangle === color;
+// };
+
+// // 10. Треугольник и квадрат одного цвета (не белого), остальные – любого цвета
+// export const validateFieldN10 = ({square, triangle}) => {
+//     return triangle === square;
+// };
